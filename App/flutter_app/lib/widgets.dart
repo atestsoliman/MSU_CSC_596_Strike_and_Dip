8bb1a7109d2dc65c364a97eb3b5f7f1b5ecb94d4
Ali Soliman <atestsoliman@gmail.com>
2018-09-24 13:16:19 -0500

    Replaced ListTile in ControlScreen with LogListTile Updated dummy data generation to match the Specification of LogListTile

--- App/flutter_app/lib/widgets.dart
+++ App/flutter_app/lib/widgets.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'dart:math';
+import 'LogListTile.dart';
 
 //ControlScreen is a view for when the app is connected to a device
 class ControlScreen extends StatefulWidget {
@@ -10,65 +11,72 @@ class ControlScreen extends StatefulWidget {
 
 class ControlScreenState extends State<ControlScreen> {
   bool _isConnected = false;
-  final _measurementList = [[0,0]];
+  final _measurementList = [
+    [0.0, 0.0, 0.0, 0, 0]
+  ];
   //For Testing
   //Todo: remove this
   final rng = Random(1234);
-
 
   //Use Flutter Blue to scan for devices
   //Currently just swaps to connected view
   //Todo: Replace with appropriate calls to FlutterBlue
   _startScan() {
-    setState(() {_isConnected = true;});
+    setState(() {
+      _isConnected = true;
+    });
   }
+
   _takeMeasurement() {
     setState(() {
-      _measurementList.add([rng.nextInt(360), rng.nextInt(180)]);
+      _measurementList.add([
+        rng.nextDouble() * 360.0,
+        rng.nextDouble() * 2.0,
+        rng.nextDouble() * 90.0,
+        rng.nextInt(3),
+        _measurementList.length
+      ]);
     });
   }
   //Todo: Improve message for null measurement
   //Todo: Consult with Matt on the formatting.
   //Todo: use tuples and/or add try/catch block
-  String _printMeasurement(_lastMeasurement) {
-      return 'N ${_lastMeasurement[0]}\u00b0, Dip ${_lastMeasurement[1]}\u00b0';
-  }
-
 
   Widget _buildControlBody() {
-    if(_isConnected) {
+    if (_isConnected) {
       //Todo: Replace with control interface.
 
-      return Column(
-        children: <Widget>[
-          Text("Connected to Device"),
-          ListBody(
-            reverse: true,
-            children: _measurementList.map((measurement) => ListTile(title: Text(_printMeasurement(measurement)))).toList(),
-          )
+      return Column(children: <Widget>[
+        Text("Connected to Device"),
+        ListBody(
+          reverse: true,
+          children: _measurementList
+              .map((measurement) => LogListTile(measurement[0], measurement[1],
+                  measurement[2], measurement[3], measurement[4]))
+              .toList(),
+        )
       ]);
-    }
-    else {
+    } else {
       //Todo: Style home screen
       return Center(
         child: Text("Welcome to GeoApp"),
       );
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Geology App")
-      ),
-      floatingActionButton: _isConnected ? FloatingActionButton(
-        child: Icon(Icons.explore),
-        onPressed: _takeMeasurement,
-      ):FloatingActionButton(
-        child: Icon(Icons.search),
-        onPressed: _startScan,
-
-      ),
+      appBar: AppBar(title: Text("Geology App")),
+      floatingActionButton: _isConnected
+          ? FloatingActionButton(
+              child: Icon(Icons.explore),
+              onPressed: _takeMeasurement,
+            )
+          : FloatingActionButton(
+              child: Icon(Icons.search),
+              onPressed: _startScan,
+            ),
       body: _buildControlBody(),
     );
   }
