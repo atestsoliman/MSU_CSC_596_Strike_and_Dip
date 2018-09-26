5ccc67d5cb43b7ab8a46b24a9e5f805e14ce7a92
Ali Soliman <atestsoliman@gmail.com>
2018-09-25 19:33:23 -0500

    Sept 25th Demo. Changed app label to GeoApp Fixed typos in LogListTile.dart

--- App/flutter_app/lib/LogListTile.dart
+++ App/flutter_app/lib/LogListTile.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
-
-class LogListTile extends StatelessWidget{
+class LogListTile extends StatelessWidget {
   final double north;
   final double sigma;
   final double dip;
@@ -11,19 +10,26 @@ class LogListTile extends StatelessWidget{
   const LogListTile(this.north, this.sigma, this.dip, this.quadrant, this.id);
 
   _displayQuad(int q) {
-    switch(q) {
-      case 0: return "NE";
-      case 1: return "SE";
-      case 2: return "SW";
-      case 3: return "NW";
+    switch (q) {
+      case 0:
+        return "NE";
+      case 1:
+        return "SE";
+      case 2:
+        return "SW";
+      case 3:
+        return "NW";
     }
   }
+
   @override
   Widget build(BuildContext context) {
-    return ListTile(title: Text('N: ${north.toStringAsFixed(2)}\u00b0 \n Uncertaniy   ${sigma.toStringAsFixed(2)}\u03c3     '
-        '\nDip: ${dip.toStringAsFixed(2)}' '\nQuad= ${_displayQuad(quadrant)}    '
-        '\nID: ${id.toString().padLeft(4, '0')}'));
+    return ListTile(
+        title: Text(
+            'N: ${north.toStringAsFixed(2)}\u00b0'
+            '\nUncertainty: ${sigma.toStringAsFixed(2)}\u03c3'
+            '\nDip: ${dip.toStringAsFixed(2)}\u00b0'
+            '\nQuadrant ${_displayQuad(quadrant)}'
+            '\nID: ${id.toString().padLeft(4, '0')}'));
   }
-
-
 }
